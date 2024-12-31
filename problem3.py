4e6d6ee484fb5a2826e7f05741ad025815d87048
Frod90 <142146516+Frod90@users.noreply.github.com>
2025-01-01 03:59:00 +0900

    first commit

---
+++ problem3.py
@@ -0,0 +1,24 @@
+
+# 반복문
+for number in range(95, 100):
+  print(number)
+
+number = 0
+while number < 10 :
+  print(number)
+  number = number + 1
+
+# 조건문
+name = "frod"
+
+if name == "frod":
+  print("name = " + name)
+else:
+  print("name != frod")
+
+
+print("--------------------------------------------")
+for test in range(10, 20):
+  print(test)
+
+print(test)
---
+++ test.py
@@ -0,0 +1,32 @@
+
+# 입력 받기
+
+# case 1 : 단순히 정수 일 때
+# number = int(input())
+
+# case 2 : 수열
+# First, Second, Third = map(int, input().split())
+# print(First, Second, Third)
+
+list1 = list(map(int, input().split()))
+print(*list1)
+
+# case 2 : 단순히 문자 일 때
+# string = input()
+
+# case 4 : 문자열
+# First, Second, Third = map(str, input().split())
+# print(First, Second, Third)
+
+list2 = list(map(str, input().split()))
+print(list2)
+
+# 계산 하기
+
+# 출력 하기
+# print(number + number)
+
+# print(string + string)
+
+
+
