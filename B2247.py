4e206f766c9e5ba6a7a6597e724e80017e270a5c
Frod90 <142146516+Frod90@users.noreply.github.com>
2025-01-07 17:56:59 +0900

    solve

--- B2247.py
+++ B2247.py
@@ -2,8 +2,8 @@
 number = int(input())
 
 answer = 0
-for i in range(2, number):
-  count = int(number / i) - 1
-  answer = answer + i * count
+for i in range(2, int(number**0.5) + 1):
+  div = number // i
+  answer += i * (div - i + 1) + (div * (div + 1) // 2) - (i * (i + 1) // 2)
 
-print(int(answer % 1_000_000))
+print(answer % 1_000_000)
