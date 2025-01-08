9e2b01295ded9dc581f6eb14d8f5faf2c5188fa1
Frod90 <142146516+Frod90@users.noreply.github.com>
2025-01-08 15:37:22 +0900

    for java

--- B1912.py
+++ B1912.py
@@ -3,8 +3,10 @@ n = int(input())
 numbers = list(map(int, input().split()))
 
 answers = numbers
+answer = numbers[0]
 
 for i in range(n - 1):
   answers[i + 1] = max(answers[i] + answers[i + 1], answers[i + 1])
+  answer = max(answer, answers[i + 1])
 
-print(max(answers))
+print(answer)
