6b0af6802e29edfd348c3773712ab23e26afd449
Frod90 <142146516+Frod90@users.noreply.github.com>
2026-04-01 14:24:31 +0900

    동전 9084

---
+++ backjun/B9084.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+  n = int(input())
+  coins = list(map(int, input().split()))
+  target = int(input())
+
+  dists = [0] * (target + 1)
+  dists[0] = 1
+
+  for coin in coins:
+    for i in range(coin, target + 1):
+      dists[i] += dists[i - coin]
+
+  print(dists[target])
