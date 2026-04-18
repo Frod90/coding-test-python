90dae7af59af08aff0daa79442aa61fdb9748822
Frod90 <142146516+Frod90@users.noreply.github.com>
2026-04-18 16:05:40 +0900

    벼락치기 14728

---
+++ backjun/B14728.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n, total_time = map(int, input().split())
+infos = [list(map(int, input().split())) for _ in range(n)]
+
+dists = [0] * (total_time + 1)
+for time, score in infos:
+  for i in range(total_time, time - 1, -1):
+    dists[i] = max(dists[i], dists[i - time] + score)
+print(dists[total_time])
